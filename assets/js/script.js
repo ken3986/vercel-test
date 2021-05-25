01c3a34c7d55056eefad277e99159155ead5f0c6
ken3986 <naka_ken8686@yahoo.co.jp>
2021-05-25 17:04:37 +0900

    VercelサーバでAPIにアクセスできるかのテスト

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
 console.log('Vercel Test')
+
+const xhr = new XMLHttpRequest()
+// xhr.open('GET', 'http://localhost:3000/api')
+xhr.open('GET', '/api')
+console.log(xhr)
+xhr.onreadystatechange = function(){
+  if(xhr.readyState == 4 && xhr.status == 200){
+    alert(xhr.responseText);
+  }
+};
+xhr.send(null);
